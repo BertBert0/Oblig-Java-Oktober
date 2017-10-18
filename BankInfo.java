5d828bfaba6c817a3eb4f4dc0071fb3e69e87173
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2017-10-18 17:45:36 +0200

    Dag 1

--- BankInfo.java
+++ BankInfo.java
@@ -1,6 +1,6 @@
 /* Leksjon 4: Obligatorisk oppgave (Bankinformasjon) 
  *******************************************************************
- * Levert av: Studentnr og navn
+ * Levert av: 216756, Robert Anholt
  *******************************************************************
  * Programmet bruker verktøyklassen 'Hjelp' som inneholder relevante
  * metoder (innlesing, avrunding) og konstanter (kurs og meny). 
@@ -45,7 +45,20 @@ public class BankInfo {
        // skriv ut beløpene avrundet til 2 siffer etter komma
        // skriv ut vekslingskursen
     // Skriv feilmelding ved ulovlig beløp
-    showMessageDialog(null, "1: Veksling");
+        double valuta = 0;
+        do { 
+           valuta = Hjelp.lesInn("Gi et dollarbeløp:");
+          if (valuta < 0)
+            showMessageDialog(null, "Ulovlig beløp!");
+        } while (valuta < 0);
+    double bUSD = Hjelp.avrund(valuta);
+    double bNOK = Hjelp.avrund(Hjelp.USD_NKR*bUSD);
+    showMessageDialog(null,  "1: Veksling" + "\n"
+                           + "____________________" + "\n"
+                           + "Beløp i USD: " + bUSD + "\n"
+                           + "Kurs: 1 USD = " + Hjelp.USD_NKR + " NOK" + "\n"
+                           + "Beløp i NOK: " + bNOK + "\n"
+                           + "____________________");
   }
 
   /* Hjelpemetode for å gi info om sparing med ett innskud som
@@ -53,15 +66,29 @@ public class BankInfo {
    */
   private static void sparing() {     
     // Les inn via kall på hjelpe-metode: et sparebeløp, en årsrente 
+    double sparebeløp = Hjelp.lesInn("Gi sparebeløp:");
+    double årsrente = Hjelp.lesInn("Gi årsrente:");
     // og et antall år i spareperioden.
+    double antallår = Hjelp.lesInn("Gi antall år:");
     // Hvis lovlige inndata
+    if (sparebeløp >0 && årsrente >0 && antallår >0)
        // skriv ut (kvitter) de aktuelle inndataene
+
        // beregn år for år: påløpt rente og ny saldo (økt beløp)
        // hvis mindre enn eller lik 10 år 
           // skriv ut år, renter og saldo
-       // Etter beregningene skrives sluttsaldo ut hvis år er over 10 
+       // Etter beregningene skrives sluttsaldo ut hvis år er over 10
+
+       showMessageDialog(null, "2: Sparing" + "\n"
+                             + "____________________" + "\n"
+                             + "Sparebeløp: " + sparebeløp + "\n"
+                             + "Årlig rente: " + årsrente + "%" + "\n"
+                             + "Antall år: " + antallår + "\n"
+                             + "____________________" + "\n"
+                             + "År     Rente     Saldo" + "\n"
+                             + "____________________" + "\n"); 
     // Skriv feilmelding ved ulovlige inndata 
-    showMessageDialog(null, "2: Sparing");
+
   } 
 
   /* Hjelpemetode for å gi info om annuitetslån, dvs. et fast årlig 
--- Hjelp.java
+++ Hjelp.java
@@ -1,31 +1,44 @@
 /* Leksjon 4: Obligatorisk oppgave (Bankinformasjon)
  *********************************************************
- * Levert av: Studentnr og navn
+ * Levert av: 216756, Robert Anholt
  *********************************************************
  * Verktøyklasse med hjelpemetoder til Bank-programmet.
  * NB! Filen lagres i mappen med BankInfo, som: Hjelp.java
  */
 
 // Evt. import av nødvendige bibliotekressurser 
+import static javax.swing.JOptionPane.*;
+import static java.lang.Double.*;
+
 
 public class Hjelp {
     // Dagens valutakurs settes utenfor bankprogrammet.
     //      final -> konstant    
     //     static -> brukes uten objekt: Klasse.KONSTANT
-  final static double USD_NKR = 0;  // Må endre verdi
+  final static double USD_NKR = 8.28;  // Må endre verdi
     // Viser aktuelle brukervalg via en tekstkonstant.
-  final static String MENY = "Vis valg (0-3)";  // Må endres
+  final static String MENY = "Konkus bank" + "\n"
+                           + "____________________" + "\n"
+                           + "Her får du info om:" + "\n"
+                           + "[1] Valutaveksling" + "\n"
+                           + "[2] Sparing" + "\n"
+                           + "[3] Lån" + "\n"
+                           + "[0] Avslutt" + "\n"
+                           + "Skriv inn tjenestekode (0-3): ";  // Må endres
 
   public static double lesInn(String ledetekst) {  	
     // Metode som ber om og leser inn en desimalverdi fra 
     // brukeren, presentert ved 'ledetekst' i inndialogvindu.
-    return 0;
+    String tallTekst = showInputDialog(ledetekst + "\n"
+                                      + "(kan være desimalverdi!)");
+    double tall = parseDouble(tallTekst);
+    return tall;
   } 
   
   public static double avrund(double tall) {
     // Metode som avrunder et desimaltall til to siffer 
     // etter komma. Se læreboka side 58.
-    return 0;
+    return (int)(tall*100 + 0.5)/100.0;
   }
 
 }
